7be9c5b75024ebd002c174e74d100a7762387ef0
Dmitri Shuralyov <shurcooL@gmail.com>
2015-10-03 01:46:38 -0700

    Minor cleanup.

--- assets/script/upload.go
+++ assets/script/upload.go
@@ -26,27 +26,21 @@ func setup() {
 	textArea.AddEventListener("paste", false, func(e dom.Event) {
 		ce := e.(*dom.ClipboardEvent)
 
-		// Get a File.
 		items := ce.Get("clipboardData").Get("items")
 		if items.Length() == 0 {
 			return
 		}
 		item := items.Index(0)
-		js.Global.Get("console").Call("log", item)
 		if item.Get("kind").String() != "file" {
 			return
 		}
 		if item.Get("type").String() != "image/png" {
 			return
 		}
-		f := item.Call("getAsFile")
-		js.Global.Get("console").Call("log", f)
+		file := item.Call("getAsFile")
 
 		go func() {
-			body := blobToBytes(f)
-			fmt.Println("got here:", len(body))
-
-			resp, err := http.Get(host + "/api/getfilename?ext=" + "png") // TODO.
+			resp, err := http.Get(host + "/api/getfilename?ext=" + "png")
 			if err != nil {
 				log.Println(err)
 				return
@@ -62,7 +56,10 @@ func setup() {
 			fmt.Println(url)
 			insertText(textArea, "![Image]("+url+")\n")
 
-			req, err := http.NewRequest("PUT", url, bytes.NewReader(body))
+			b := blobToBytes(file)
+			fmt.Println("file size:", len(b))
+
+			req, err := http.NewRequest("PUT", url, bytes.NewReader(b))
 			if err != nil {
 				log.Println(err)
 				return
@@ -74,6 +71,7 @@ func setup() {
 				return
 			}
 			_ = resp.Body.Close()
+
 			fmt.Println("done")
 		}()
 	})
@@ -89,8 +87,8 @@ func insertText(t *dom.HTMLTextAreaElement, inserted string) {
 func blobToBytes(blob *js.Object) []byte {
 	var b = make(chan []byte)
 	fileReader := js.Global.Get("FileReader").New()
-	fileReader.Set("onload", func(e *js.Object) {
-		b <- js.Global.Get("Uint8Array").New(e.Get("target").Get("result")).Interface().([]byte)
+	fileReader.Set("onload", func() {
+		b <- js.Global.Get("Uint8Array").New(fileReader.Get("result")).Interface().([]byte)
 	})
 	fileReader.Call("readAsArrayBuffer", blob)
 	return <-b
