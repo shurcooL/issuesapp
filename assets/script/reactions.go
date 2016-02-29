77985a92f5f6e007bb836127d9fa12fcaba1a852
Dmitri Shuralyov <shurcooL@gmail.com>
2016-02-29 02:40:10 -0800

    WIP: Complete most of reactions rendering.

---
+++ assets/script/reactions.go
@@ -0,0 +1,13 @@
+// +build js
+
+package main
+
+import (
+	"fmt"
+
+	"honnef.co/go/js/dom"
+)
+
+func ShowReactionMenu(this dom.HTMLElement, commentID uint64) {
+	fmt.Println("TODO: ShowReactionMenu not implemented")
+}
