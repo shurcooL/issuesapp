ea06750c912eca1742c782c63e29cb46175160c3
Dmitri Shuralyov <shurcooL@gmail.com>
2016-08-27 23:13:37 -0700

    Link to canonical issue tracker more visibly.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Package issuesapp is a web frontend for an issues service.
 
+Note, the canonical issue tracker for this package is currently hosted at
+https://dmitri.shuralyov.com/issues/github.com/shurcooL/issuesapp.
+It is implemented using this very package.
+
 Installation
 ------------
 
--- doc.go
+++ doc.go
@@ -1,4 +1,8 @@
 //go:generate vfsgendev -source="github.com/shurcooL/issuesapp".Assets
 
 // Package issuesapp is a web frontend for an issues service.
+//
+// Note, the canonical issue tracker for this package is currently hosted at
+// https://dmitri.shuralyov.com/issues/github.com/shurcooL/issuesapp.
+// It is implemented using this very package.
 package issuesapp
