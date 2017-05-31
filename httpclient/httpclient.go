adb5259c82adaf35f7d46246a169005472fbe690
Dmitri Shuralyov <shurcooL@gmail.com>
2017-05-30 22:20:12 -0400

    httpclient: Use httproute routes.
    This replaces previously hardcoded and duplicate paths with the single
    source of truth from httproute package.

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -11,16 +11,16 @@ import (
 
 	"github.com/shurcooL/go/ctxhttp"
 	"github.com/shurcooL/issues"
+	"github.com/shurcooL/issuesapp/httproute"
 )
 
 // NewIssues creates a client that implements issues.Service remotely over HTTP.
 // scheme and host can be empty strings to target local service.
 func NewIssues(scheme, host string) *Issues {
 	return &Issues{
-		issuesURL: &url.URL{
+		baseURL: &url.URL{
 			Scheme: scheme,
 			Host:   host,
-			Path:   "/api/" + "issues/",
 		},
 	}
 }
@@ -28,19 +28,18 @@ func NewIssues(scheme, host string) *Issues {
 // Issues implements issues.Service remotely over HTTP.
 // Use NewIssues for creation, zero value of Issues is unfit for use.
 type Issues struct {
-	// Base URL for API requests, including common "issues/" prefix for issues endpoints.
-	issuesURL *url.URL
+	baseURL *url.URL // Base URL for API requests.
 }
 
 func (i *Issues) List(ctx context.Context, repo issues.RepoSpec, opt issues.IssueListOptions) ([]issues.Issue, error) {
 	u := url.URL{
-		Path: "list",
+		Path: httproute.List,
 		RawQuery: url.Values{
 			"RepoURI":  {repo.URI},
 			"OptState": {string(opt.State)},
 		}.Encode(),
 	}
-	resp, err := ctxhttp.Get(ctx, nil, i.issuesURL.ResolveReference(&u).String())
+	resp, err := ctxhttp.Get(ctx, nil, i.baseURL.ResolveReference(&u).String())
 	if err != nil {
 		return nil, err
 	}
@@ -56,13 +55,13 @@ func (i *Issues) List(ctx context.Context, repo issues.RepoSpec, opt issues.Issu
 
 func (i *Issues) Count(ctx context.Context, repo issues.RepoSpec, opt issues.IssueListOptions) (uint64, error) {
 	u := url.URL{
-		Path: "count",
+		Path: httproute.Count,
 		RawQuery: url.Values{
 			"RepoURI":  {repo.URI},
 			"OptState": {string(opt.State)},
 		}.Encode(),
 	}
-	resp, err := ctxhttp.Get(ctx, nil, i.issuesURL.ResolveReference(&u).String())
+	resp, err := ctxhttp.Get(ctx, nil, i.baseURL.ResolveReference(&u).String())
 	if err != nil {
 		return 0, err
 	}
@@ -90,10 +89,10 @@ func (i *Issues) ListComments(ctx context.Context, repo issues.RepoSpec, id uint
 		q.Set("Opt.Length", fmt.Sprint(opt.Length))
 	}
 	u := url.URL{
-		Path:     "list-comments",
+		Path:     httproute.ListComments,
 		RawQuery: q.Encode(),
 	}
-	resp, err := ctxhttp.Get(ctx, nil, i.issuesURL.ResolveReference(&u).String())
+	resp, err := ctxhttp.Get(ctx, nil, i.baseURL.ResolveReference(&u).String())
 	if err != nil {
 		return nil, err
 	}
@@ -125,7 +124,7 @@ func (*Issues) Edit(_ context.Context, repo issues.RepoSpec, id uint64, ir issue
 
 func (i *Issues) EditComment(ctx context.Context, repo issues.RepoSpec, id uint64, cr issues.CommentRequest) (issues.Comment, error) {
 	u := url.URL{
-		Path: "edit-comment",
+		Path: httproute.EditComment,
 		RawQuery: url.Values{
 			"RepoURI": {repo.URI},
 			"ID":      {fmt.Sprint(id)},
@@ -140,7 +139,7 @@ func (i *Issues) EditComment(ctx context.Context, repo issues.RepoSpec, id uint6
 	if cr.Reaction != nil {
 		data.Set("Reaction", string(*cr.Reaction))
 	}
-	resp, err := ctxhttp.PostForm(ctx, nil, i.issuesURL.ResolveReference(&u).String(), data)
+	resp, err := ctxhttp.PostForm(ctx, nil, i.baseURL.ResolveReference(&u).String(), data)
 	if err != nil {
 		return issues.Comment{}, err
 	}
