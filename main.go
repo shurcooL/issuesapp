888710ee8d3ebe675b961ecd6220db39e07735e9
Dmitri Shuralyov <shurcooL@gmail.com>
2017-07-30 01:04:42 -0400

    Rearrange code for better readability.

--- main.go
+++ main.go
@@ -32,48 +32,6 @@ import (
 	"golang.org/x/net/html"
 )
 
-// contextKey is a value for use with context.WithValue. It's used as
-// a pointer so it fits in an interface{} without allocation.
-type contextKey struct {
-	name string
-}
-
-func (k *contextKey) String() string { return "github.com/shurcooL/issuesapp context value " + k.name }
-
-// RepoSpecContextKey is a context key for the request's issues.RepoSpec.
-// That value specifies which repo the issues are to be displayed for.
-// The associated value will be of type issues.RepoSpec.
-var RepoSpecContextKey = &contextKey{"RepoSpec"}
-
-// BaseURIContextKey is a context key for the request's base URI.
-// That value specifies the base URI prefix to use for all absolute URLs.
-// The associated value will be of type string.
-var BaseURIContextKey = &contextKey{"BaseURI"}
-
-// Options for configuring issues app.
-type Options struct {
-	Notifications    notifications.Service // If not nil, issues containing unread notifications are highlighted.
-	DisableReactions bool                  // Disable all support for displaying and toggling reactions.
-
-	HeadPre, HeadPost template.HTML
-	BodyPre           string // An html/template definition of "body-pre" template.
-
-	// BodyTop provides components to include on top of <body> of page rendered for req. It can be nil.
-	BodyTop func(req *http.Request) ([]htmlg.Component, error)
-}
-
-type handler struct {
-	http.Handler
-
-	is issues.Service
-	us users.Service
-
-	// static is loaded once in New, and is only for rendering templates that don't use state.
-	static *template.Template
-
-	Options
-}
-
 // TODO: Find a better way for issuesapp to be able to ensure registration of a top-level route:
 //
 // 	emojisHandler := httpgzip.FileServer(emojis.Assets, httpgzip.FileServerOptions{ServeError: httpgzip.Detailed})
@@ -123,13 +81,13 @@ func New(service issues.Service, usersService users.Service, opt Options) http.H
 	r := mux.NewRouter()
 	// TODO: Make redirection work.
 	//r.StrictSlash(true) // THINK: Can't use this due to redirect not taking baseURI into account.
-	r.HandleFunc("/", handler.issuesHandler).Methods("GET")
-	r.HandleFunc("/{id:[0-9]+}", handler.issueHandler).Methods("GET")
-	r.HandleFunc("/{id:[0-9]+}/edit", handler.postEditIssueHandler).Methods("POST")
-	r.HandleFunc("/{id:[0-9]+}/comment", handler.postCommentHandler).Methods("POST")
-	r.HandleFunc("/{id:[0-9]+}/comment/{commentID:[0-9]+}", handler.postEditCommentHandler).Methods("POST")
-	r.HandleFunc("/new", handler.createIssueHandler).Methods("GET")
-	r.HandleFunc("/new", handler.postCreateIssueHandler).Methods("POST")
+	r.HandleFunc("/", handler.IssuesHandler).Methods("GET")
+	r.HandleFunc("/{id:[0-9]+}", handler.IssueHandler).Methods("GET")
+	r.HandleFunc("/{id:[0-9]+}/edit", handler.PostEditIssueHandler).Methods("POST")
+	r.HandleFunc("/{id:[0-9]+}/comment", handler.PostCommentHandler).Methods("POST")
+	r.HandleFunc("/{id:[0-9]+}/comment/{commentID:[0-9]+}", handler.PostEditCommentHandler).Methods("POST")
+	r.HandleFunc("/new", handler.CreateIssueHandler).Methods("GET")
+	r.HandleFunc("/new", handler.PostCreateIssueHandler).Methods("POST")
 	h := http.NewServeMux()
 	h.Handle("/", r)
 	assetsFileServer := httpgzip.FileServer(assets.Assets, httpgzip.FileServerOptions{ServeError: httpgzip.Detailed})
@@ -140,6 +98,48 @@ func New(service issues.Service, usersService users.Service, opt Options) http.H
 	handler.Handler = h
 	return handler
 }
+
+type handler struct {
+	http.Handler
+
+	is issues.Service
+	us users.Service
+
+	// static is loaded once in New, and is only for rendering templates that don't use state.
+	static *template.Template
+
+	Options
+}
+
+// Options for configuring issues app.
+type Options struct {
+	Notifications    notifications.Service // If not nil, issues containing unread notifications are highlighted.
+	DisableReactions bool                  // Disable all support for displaying and toggling reactions.
+
+	HeadPre, HeadPost template.HTML
+	BodyPre           string // An html/template definition of "body-pre" template.
+
+	// BodyTop provides components to include on top of <body> of page rendered for req. It can be nil.
+	BodyTop func(req *http.Request) ([]htmlg.Component, error)
+}
+
+// RepoSpecContextKey is a context key for the request's issues.RepoSpec.
+// That value specifies which repo the issues are to be displayed for.
+// The associated value will be of type issues.RepoSpec.
+var RepoSpecContextKey = &contextKey{"RepoSpec"}
+
+// BaseURIContextKey is a context key for the request's base URI.
+// That value specifies the base URI prefix to use for all absolute URLs.
+// The associated value will be of type string.
+var BaseURIContextKey = &contextKey{"BaseURI"}
+
+// contextKey is a value for use with context.WithValue. It's used as
+// a pointer so it fits in an interface{} without allocation.
+type contextKey struct {
+	name string
+}
+
+func (k *contextKey) String() string { return "github.com/shurcooL/issuesapp context value " + k.name }
 
 func loadTemplates(state common.State, bodyPre string) (*template.Template, error) {
 	t := template.New("").Funcs(template.FuncMap{
@@ -201,6 +201,216 @@ func loadTemplates(state common.State, bodyPre string) (*template.Template, erro
 		return nil, err
 	}
 	return t.New("body-pre").Parse(bodyPre)
+}
+
+func (h *handler) IssuesHandler(w http.ResponseWriter, req *http.Request) {
+	state, err := h.state(req)
+	if err != nil {
+		log.Println("state:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err = h.static.ExecuteTemplate(w, "issues.html.tmpl", &state)
+	if err != nil {
+		log.Println("t.ExecuteTemplate:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *handler) IssueHandler(w http.ResponseWriter, req *http.Request) {
+	state, err := h.state(req)
+	if err != nil {
+		log.Println("state:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Call loadTemplates to set updated reactionsBar, etc., template functions.
+	t, err := loadTemplates(state.State, h.Options.BodyPre)
+	if err != nil {
+		log.Println("loadTemplates:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var buf bytes.Buffer
+	err = t.ExecuteTemplate(&buf, "issue.html.tmpl", &state)
+	if err != nil && (strings.Contains(err.Error(), "no such file or directory") || strings.Contains(err.Error(), "does not exist")) { // TODO: Better error handling.
+		log.Println("t.ExecuteTemplate:", err)
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		log.Println("t.ExecuteTemplate:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.Copy(w, &buf)
+}
+
+func (h *handler) CreateIssueHandler(w http.ResponseWriter, req *http.Request) {
+	state, err := h.state(req)
+	if err != nil {
+		log.Println("state:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if state.CurrentUser.ID == 0 {
+		http.Error(w, "this page requires an authenticated user", http.StatusUnauthorized)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err = h.static.ExecuteTemplate(w, "new-issue.html.tmpl", &state)
+	if err != nil {
+		log.Println("t.ExecuteTemplate:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *handler) PostCreateIssueHandler(w http.ResponseWriter, req *http.Request) {
+	baseURI := req.Context().Value(BaseURIContextKey).(string)
+	repoSpec := req.Context().Value(RepoSpecContextKey).(issues.RepoSpec)
+
+	var issue issues.Issue
+	err := json.NewDecoder(req.Body).Decode(&issue)
+	if err != nil {
+		log.Println("json.Decode:", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	issue, err = h.is.Create(req.Context(), repoSpec, issue)
+	if err != nil {
+		log.Println("is.Create:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%s/%d", baseURI, issue.ID)
+}
+
+func (h *handler) PostEditIssueHandler(w http.ResponseWriter, req *http.Request) {
+	if err := req.ParseForm(); err != nil {
+		log.Println("req.ParseForm:", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	vars := mux.Vars(req)
+	repoSpec := req.Context().Value(RepoSpecContextKey).(issues.RepoSpec)
+
+	var ir issues.IssueRequest
+	err := json.Unmarshal([]byte(req.PostForm.Get("value")), &ir)
+	if err != nil {
+		log.Println("PostEditIssueHandler: json.Unmarshal value:", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	issue, events, err := h.is.Edit(req.Context(), repoSpec, uint64(mustAtoi(vars["id"])), ir)
+	if err != nil {
+		log.Println("is.Edit:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = func(w io.Writer, issue issues.Issue) error {
+		var resp = make(url.Values)
+
+		var buf bytes.Buffer
+		err := htmlg.RenderComponents(&buf, component.IssueStateBadge{Issue: issue})
+		if err != nil {
+			return err
+		}
+		resp.Set("issue-state-badge", buf.String())
+
+		buf.Reset()
+		err = h.static.ExecuteTemplate(&buf, "toggle-button", issue.State)
+		if err != nil {
+			return err
+		}
+		resp.Set("issue-toggle-button", buf.String())
+
+		for _, e := range events {
+			buf.Reset()
+			err = h.static.ExecuteTemplate(&buf, "event", event{e})
+			if err != nil {
+				return err
+			}
+			resp.Add("new-event", buf.String())
+		}
+
+		_, err = io.WriteString(w, resp.Encode())
+		return err
+	}(w, issue)
+	if err != nil {
+		log.Println("t.ExecuteTemplate:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *handler) PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	if err := req.ParseForm(); err != nil {
+		log.Println("req.ParseForm:", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	vars := mux.Vars(req)
+	repoSpec := req.Context().Value(RepoSpecContextKey).(issues.RepoSpec)
+
+	comment := issues.Comment{
+		Body: req.PostForm.Get("value"),
+	}
+
+	issueID := uint64(mustAtoi(vars["id"]))
+	comment, err := h.is.CreateComment(req.Context(), repoSpec, issueID, comment)
+	if err != nil {
+		log.Println("is.CreateComment:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = h.static.ExecuteTemplate(w, "comment", comment)
+	if err != nil {
+		log.Println("t.ExecuteTemplate:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *handler) PostEditCommentHandler(w http.ResponseWriter, req *http.Request) {
+	if err := req.ParseForm(); err != nil {
+		log.Println("req.ParseForm:", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	vars := mux.Vars(req)
+	repoSpec := req.Context().Value(RepoSpecContextKey).(issues.RepoSpec)
+
+	body := req.PostForm.Get("value")
+	cr := issues.CommentRequest{
+		ID:   uint64(mustAtoi(vars["commentID"])),
+		Body: &body,
+	}
+
+	_, err := h.is.EditComment(req.Context(), repoSpec, uint64(mustAtoi(vars["id"])), cr)
+	if err != nil {
+		log.Println("is.EditComment:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func mustAtoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
 }
 
 func (h *handler) state(req *http.Request) (state, error) {
@@ -351,14 +561,6 @@ func (s state) augmentUnread(dis []issue) []issue {
 	return dis
 }
 
-func mustAtoi(s string) int {
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		panic(err)
-	}
-	return i
-}
-
 func (s state) Issue() (issues.Issue, error) {
 	return s.is.Get(s.req.Context(), s.RepoSpec, s.IssueID)
 }
@@ -389,206 +591,4 @@ func (s state) Items() ([]issueItem, error) {
 func (s state) ForceIssuesApp() bool {
 	forceIssuesApp, _ := strconv.ParseBool(s.req.URL.Query().Get("issuesapp"))
 	return forceIssuesApp
-}
-
-func (h *handler) issuesHandler(w http.ResponseWriter, req *http.Request) {
-	state, err := h.state(req)
-	if err != nil {
-		log.Println("state:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err = h.static.ExecuteTemplate(w, "issues.html.tmpl", &state)
-	if err != nil {
-		log.Println("t.ExecuteTemplate:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func (h *handler) issueHandler(w http.ResponseWriter, req *http.Request) {
-	state, err := h.state(req)
-	if err != nil {
-		log.Println("state:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	// Call loadTemplates to set updated reactionsBar, etc., template functions.
-	t, err := loadTemplates(state.State, h.Options.BodyPre)
-	if err != nil {
-		log.Println("loadTemplates:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	var buf bytes.Buffer
-	err = t.ExecuteTemplate(&buf, "issue.html.tmpl", &state)
-	if err != nil && (strings.Contains(err.Error(), "no such file or directory") || strings.Contains(err.Error(), "does not exist")) { // TODO: Better error handling.
-		log.Println("t.ExecuteTemplate:", err)
-		http.Error(w, "404 Not Found", http.StatusNotFound)
-		return
-	} else if err != nil {
-		log.Println("t.ExecuteTemplate:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.Copy(w, &buf)
-}
-
-func (h *handler) createIssueHandler(w http.ResponseWriter, req *http.Request) {
-	state, err := h.state(req)
-	if err != nil {
-		log.Println("state:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if state.CurrentUser.ID == 0 {
-		http.Error(w, "this page requires an authenticated user", http.StatusUnauthorized)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err = h.static.ExecuteTemplate(w, "new-issue.html.tmpl", &state)
-	if err != nil {
-		log.Println("t.ExecuteTemplate:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func (h *handler) postCreateIssueHandler(w http.ResponseWriter, req *http.Request) {
-	baseURI := req.Context().Value(BaseURIContextKey).(string)
-	repoSpec := req.Context().Value(RepoSpecContextKey).(issues.RepoSpec)
-
-	var issue issues.Issue
-	err := json.NewDecoder(req.Body).Decode(&issue)
-	if err != nil {
-		log.Println("json.Decode:", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	issue, err = h.is.Create(req.Context(), repoSpec, issue)
-	if err != nil {
-		log.Println("is.Create:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(w, "%s/%d", baseURI, issue.ID)
-}
-
-func (h *handler) postEditIssueHandler(w http.ResponseWriter, req *http.Request) {
-	if err := req.ParseForm(); err != nil {
-		log.Println("req.ParseForm:", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	vars := mux.Vars(req)
-	repoSpec := req.Context().Value(RepoSpecContextKey).(issues.RepoSpec)
-
-	var ir issues.IssueRequest
-	err := json.Unmarshal([]byte(req.PostForm.Get("value")), &ir)
-	if err != nil {
-		log.Println("postEditIssueHandler: json.Unmarshal value:", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	issue, events, err := h.is.Edit(req.Context(), repoSpec, uint64(mustAtoi(vars["id"])), ir)
-	if err != nil {
-		log.Println("is.Edit:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = func(w io.Writer, issue issues.Issue) error {
-		var resp = make(url.Values)
-
-		var buf bytes.Buffer
-		err := htmlg.RenderComponents(&buf, component.IssueStateBadge{Issue: issue})
-		if err != nil {
-			return err
-		}
-		resp.Set("issue-state-badge", buf.String())
-
-		buf.Reset()
-		err = h.static.ExecuteTemplate(&buf, "toggle-button", issue.State)
-		if err != nil {
-			return err
-		}
-		resp.Set("issue-toggle-button", buf.String())
-
-		for _, e := range events {
-			buf.Reset()
-			err = h.static.ExecuteTemplate(&buf, "event", event{e})
-			if err != nil {
-				return err
-			}
-			resp.Add("new-event", buf.String())
-		}
-
-		_, err = io.WriteString(w, resp.Encode())
-		return err
-	}(w, issue)
-	if err != nil {
-		log.Println("t.ExecuteTemplate:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func (h *handler) postCommentHandler(w http.ResponseWriter, req *http.Request) {
-	if err := req.ParseForm(); err != nil {
-		log.Println("req.ParseForm:", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	vars := mux.Vars(req)
-	repoSpec := req.Context().Value(RepoSpecContextKey).(issues.RepoSpec)
-
-	comment := issues.Comment{
-		Body: req.PostForm.Get("value"),
-	}
-
-	issueID := uint64(mustAtoi(vars["id"]))
-	comment, err := h.is.CreateComment(req.Context(), repoSpec, issueID, comment)
-	if err != nil {
-		log.Println("is.CreateComment:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = h.static.ExecuteTemplate(w, "comment", comment)
-	if err != nil {
-		log.Println("t.ExecuteTemplate:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func (h *handler) postEditCommentHandler(w http.ResponseWriter, req *http.Request) {
-	if err := req.ParseForm(); err != nil {
-		log.Println("req.ParseForm:", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	vars := mux.Vars(req)
-	repoSpec := req.Context().Value(RepoSpecContextKey).(issues.RepoSpec)
-
-	body := req.PostForm.Get("value")
-	cr := issues.CommentRequest{
-		ID:   uint64(mustAtoi(vars["commentID"])),
-		Body: &body,
-	}
-
-	_, err := h.is.EditComment(req.Context(), repoSpec, uint64(mustAtoi(vars["id"])), cr)
-	if err != nil {
-		log.Println("is.EditComment:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 }
